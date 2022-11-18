193ec927ad333dfc6a6719db790bb71c717ebd83
TaiyebShikdar <c1012233@my.shu.ac.uk>
2022-11-18 14:14:18 +0000

    added nav.js

---
+++ js/nav.js
@@ -0,0 +1,20 @@
+//I I F E
+
+(function(){
+    console.dir(window.location)
+
+    let url = window.location.href
+    console.info(url)
+
+    let filename = url.split("/").pop()
+    console.info(filename)
+
+
+
+
+
+
+
+
+
+})()
